0cf5aa5b91c412c91977bed4511160286ec15332
beautifulgirlxuxu <804316947@qq.com>
2018-04-01 22:26:57 +0800

    继承方式总复习

--- view/inherit.js
+++ view/inherit.js
@@ -1,13 +1,13 @@
 //定义一个父类
-function People(name)  {
-    this.name = name || 'People';
-    this.sleep = function() {
-        console.log(this.name + '正在睡觉！')
-    }
-}
-People.prototype.eat = function(food) {
-    console.log(this.name + '正在吃' + food)
-}
+// function People(name)  {
+//     this.name = name || 'People';
+//     this.sleep = function() {
+//         console.log(this.name + '正在睡觉！')
+//     }
+// }
+// People.prototype.eat = function(food) {
+//     console.log(this.name + '正在吃' + food)
+// }
 
 //原型链继承
 // 核心： 将父类的实例作为子类的原型
@@ -41,14 +41,78 @@ People.prototype.eat = function(food) {
 
 // 实例继承
 // 核心：为父类实例添加新特性，作为子类实例返回
-function Qing(name) {
-    var insatnce = new People();
-    insatnce.name = name || '徐燕青'
+// function Qing(name) {
+//     var insatnce = new People();
+//     insatnce.name = name || '徐燕青'
+// }
+
+// var qing = new Qing();
+// console.log(qing.name);
+// // console.log(qing.sleep());
+// // console.log(qing.eat());
+// console.log(qing instanceof People);
+// console.log(qing instanceof Qing)
+
+// 构造函数绑定，将父对象的构造函数通过call、apply绑定到子对象中
+function Animal() {
+    // this.species = '动物';
+}
+Animal.prototype.species = '动物';
+function Cat(name, color) {
+    // Animal.call(this)
+    this.name = name;
+    this.color = color;
+}
+// Cat.prototype = new Animal();
+// // Cat.prototype = Animal.prototype;
+// Cat.prototype.constructor = Cat;
+function extend(parent, child) {
+    // var F = function(){};
+    // F.prototype = parent.prototype;
+    // child.prototype = new F();
+    // child.prototype.constructor = child;
+    var p = parent.prototype;
+    var c = child.prototype;
+    for(i in p) {
+        c[i] = p[i];
+    }
+    c.uber = p;
+}
+extend(Animal, Cat)
+
+var cat1 = new Cat('小花', '白色')
+// console.log(cat1.name + cat1.color + '属于' + cat1.species);
+// console.log(cat1.constructor === Cat);
+// console.log(cat1.constructor === Animal);
+
+var Chinese = {
+    nation: '中国'
+}
+// var Docter = {
+//     career: '医生'
+// }
+
+// function object(o) {
+//     function F() {}
+//         F.prototype = o;
+//         return new F();
+// }
+Chinese.birthPlaces = ['北京','上海','香港'];
+function extendCopy(p, c) {
+    var c = c || {};
+    for (var i in p) {
+　　  if (typeof p[i] === 'object') {
+    　　　    c[i] = (p[i].constructor === Array) ? [] : {};
+    　　　　　extendCopy(p[i], c[i]);
+　　　} else {
+　　　　　　　　　c[i] = p[i];
+　　　　　　}
+　　　　}
+    return c;
 }
 
-var qing = new Qing();
-console.log(qing.name);
-// console.log(qing.sleep());
-// console.log(qing.eat());
-console.log(qing instanceof People);
-console.log(qing instanceof Qing)
+var Docter = extendCopy(Chinese);
+Docter.birthPlaces.push ('厦门');
+console.log(Docter.birthPlaces);
+console.log(Chinese.birthPlaces);
+// console.log(Docter.prototype.constructor);
