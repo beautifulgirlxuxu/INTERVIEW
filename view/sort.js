9574d201a6fc332ff9b5b721d567f8fa9f5219c1
beautifulgirlxuxu <804316947@qq.com>
2018-04-08 22:22:28 +0800

    链家笔试

---
+++ view/sort.js
@@ -0,0 +1,12 @@
+function sort(arr) {
+    var length = arr.length;
+    for(var i=0; i<length; i++) {
+        if (arr[i+1] - arr[i] > 1) {
+            console.log("漏掉的数是" + (arr[i] + 1))
+        }
+    }
+    return arr;
+}
+
+var arr1 = [1,3,4,5,7,9];
+console.log(sort(arr1));
