640759aacbd43f2d37ee98e46038351dfea8ad45
beautifulgirlxuxu <804316947@qq.com>
2018-04-11 23:11:33 +0800

    三数之和

---
+++ view/sumOfThree.js
@@ -0,0 +1,28 @@
+function sumOfThree(arr) {
+    if(arr.length < 3) {
+        return ;
+    } else {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// var x
+// var mycars = new Array()
+// mycars[0] = "Saab"
+// mycars[1] = "Volvo"
+// mycars[2] = "BMW"
+// for (x in mycars)
+// {
+// console.log(mycars[x])
+// }
