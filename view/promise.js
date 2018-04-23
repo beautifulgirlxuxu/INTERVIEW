8b33f4f3c6e36b8e8f5d4364ab53e6bbf043f5b4
beautifulgirlxuxu <804316947@qq.com>
2018-04-23 22:17:27 +0800

    promise学习

---
+++ view/promise.js
@@ -0,0 +1,16 @@
+// Promise就是一个容器，里面放着未来发生的一件事的结果
+// 三个状态pending,resolved,rejected,只有异步操作才能改变状态
+// then函数为promise实例添加状态改变的回调函数
+// catch函数用于指定发生错误时的函数
+function asyncRun(data) {
+    var p = new Promise(function(resolve,reject) {
+        if(data) {
+            resolve(data);
+        } else {
+            reject(reason);
+        }
+    })
+    return p;
+}
+
+asyncRun(3);
