7a36840fa3e5293e0235822a6aa8915d03905ea7
beautifulgirlxuxu <804316947@qq.com>
2018-04-25 21:53:22 +0800

    promise学习

--- view/promise.js
+++ view/promise.js
@@ -2,6 +2,7 @@
 // 三个状态pending,resolved,rejected,只有异步操作才能改变状态
 // then函数为promise实例添加状态改变的回调函数
 // catch函数用于指定发生错误时的函数
+// 让异步操作以同步操作的形式表现出来
 function asyncRun(data) {
     var p = new Promise(function(resolve,reject) {
         if(data) {
