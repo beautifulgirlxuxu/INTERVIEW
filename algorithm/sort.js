1819de282baf800063a61f5b94210bb163413835
beautifulgirlxuxu <804316947@qq.com>
2018-05-06 22:40:39 +0800

    数组sort方法

--- algorithm/sort.js
+++ algorithm/sort.js
@@ -1,20 +1,9 @@
-function sort(arr) {
-    var length = arr.length;
-    for(var i=0; i<length; i++) {
-        if (arr[i+1] - arr[i] > 1) {
-            console.log("漏掉的数是" + (arr[i] + 1))
-        }
-    }
-    return arr;
-}
+// Array.sort()
+// 不提供参数，按字母顺序排序
+// 正常排序，提供两个参数，a,b，返回一个体现元素排序位置的值
+var arr = [21,3,2,12,55,43,99,19]
 
-var arr1 = [1,3,4,5,7,9];
-// console.log(sort(arr1));
-
-
-// function br(a,b,c) {
-//     arguments[2] = 10;
-//     console.log(c);
-// }
-
-// br(1,2,3)
+var arrSort = arr.sort(function(param1,param2) {
+    return param1 -param2;
+})
+console.log(arrSort);
