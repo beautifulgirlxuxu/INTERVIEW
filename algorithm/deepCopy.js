5b9b2b149f22a0f66403fa020458292152794d20
beautifulgirlxuxu <804316947@qq.com>
2018-05-18 23:07:35 +0800

    对象深拷贝

--- algorithm/deepCopy.js
+++ algorithm/deepCopy.js
@@ -10,15 +10,25 @@ function deepCopy (source) {
 
 // 先使用内置的JSON.stringify()函数，将其转化为数组。此时生成的字符串已经和原对象没有任何联系了
 // 再通过JSON.parse()函数，将生成的字符串转化为一个新的对象
-function deepCopy(o){
+// 缺点：IE8以下兼容问题,function不能copy过来
+// JSON.parse()解析json格式的字符串将其转化成对象
+
+function deepCopy1(o){
     return JSON.parse(JSON.stringify(o));
 }
 
 var obj = {
     name: 'xuyanqing',
     age: 21,
-    sex: 'female'
+    sex: {
+        1: 'male',
+        2: 'female',
+    },
+    func: function () {}
 }
+var obj1 = [1,2,3,4,5];
 
-var copyObj = deepCopy(obj);
+var copyObj = deepCopy1(obj);
+// copyObj.splice(0,2,8);
 console.log(copyObj);
+// console.log(obj1);
